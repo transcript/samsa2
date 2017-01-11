88c4719d3edd4138a785cdaa9a48f9c56f9fe93b
transcript <swestreich@gmail.com>
2017-01-10 16:47:42 -0800

    cleaning up R scripts, optimizing, more README

--- DIAMOND_analysis_counter_v2.py
+++ DIAMOND_analysis_counter_v2.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env Python
-
+##########################################################################
+#
+# Copyright (C) 2015-2016 Sam Westreich
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation;
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##########################################################################
+#
 # DIAMOND_analysis_counter.py
-# Created 8/16/2016, by Sam Westreich, swestreich@gmail.com, github.com/transcript
-
-# Purpose: This parses through the results file from a DIAMOND annotation run (in BLAST
-# m8 format) to get the results into something a bit more compressed and readable.
+# Created 8/16/2016, this version created 1/10/2017
+# Sam Westreich, stwestreich@ucdavis.edu, github.com/transcript
+#
+# This program parses through the results file from a DIAMOND annotation run
+# (in BLAST m8 format) to get the results into something more compressed
+# and readable.
+#
+# Usage: 
+#
+# -I		infile			specifies the infile (a DIAMOND results file 
+#								in m8 format)
+# -D		database		specifies a reference database to search against 
+#								for results
+# -O		organism		returns organism results
+# -F		function		returns functional results
+# 
+##########################################################################
 
 # imports
 import operator, sys, time, gzip, re
-
-# usage statement
-# TO COME LATER ***************************
 
 # String searching function:
 def string_find(usage_term):
@@ -22,8 +50,13 @@ def string_find(usage_term):
 
 t0 = time.clock()
 
+# checking for an option (organism or function) to be specified
+if "-O" not in sys.argv.upper():
+	if "-F" not in sys.argv.upper():
+		sys.exit("WARNING: need to specify either organism results (with -O flag in command) or functional results (with -F flag in command).")
+
 # loading starting file
-if "-i" in sys.argv:
+if "-i" in sys.argv.lower():
 	infile_name = string_find("-i")
 else:
 	sys.exit ("WARNING: infile must be specified using '-i' flag.")
@@ -60,15 +93,15 @@ print "Time elapsed: " + str(t1-t0) + " seconds."
 infile.close()
 
 # temporary outfile
-temp_outfile_name = infile_name[:-5] + ".out"
-temp_outfile  = open (temp_outfile_name, "w")
-for k, v in sorted(RefSeq_hit_count_db.items(), key=lambda (k,v): -v):
-	q = v * 100 / float(line_counter)
-	temp_outfile.write (str(q) + "%\t" + str(v) + "\t" + k + "\n")
+#temp_outfile_name = infile_name[:-5] + ".out"
+#temp_outfile  = open (temp_outfile_name, "w")
+#for k, v in sorted(RefSeq_hit_count_db.items(), key=lambda (k,v): -v):
+#	q = v * 100 / float(line_counter)
+#	temp_outfile.write (str(q) + "%\t" + str(v) + "\t" + k + "\n")
+#
+#print "Results are saved in " + temp_outfile_name + " for quick resuming."
 
-print "Results are saved in " + temp_outfile_name + " for quick resuming."
-
-temp_outfile.close()
+#temp_outfile.close()
 
 #print ("\nTop ten matches:")
 #for k, v in sorted(RefSeq_hit_count_db.items(), key=lambda (k,v): -v)[:10]:
@@ -76,7 +109,7 @@ temp_outfile.close()
 #	print (str(q) + "%\t" + str(v) + "\t" + k )
 	
 # time to search for these in the reference database
-if "-d" in sys.argv:
+if "-d" in sys.argv.lower():
 	db_name = string_find("-d")
 else:
 	sys.exit( "No database file indicated; skipping database search step.")
@@ -88,8 +121,10 @@ print "\nStarting database analysis now."
 t2 = time.clock()
 
 # building a dictionary of the reference database
-db_func_dictionary = {}
-db_org_dictionary = {}
+if "-F" in sys.argv.upper():
+	db_func_dictionary = {}
+if "-O" in sys.argv.upper():
+	db_org_dictionary = {}
 db_line_counter = 0
 db_error_counter = 0
 
@@ -125,7 +160,6 @@ for line in db:
 							db_org = split_db_org[1] + " " + split_db_org[2]
 						print line
 						print db_org
-						#continue
 		else:	
 			db_org = line.split("[", 1)
 			db_org = db_org[1].split()
@@ -133,18 +167,19 @@ for line in db:
 				db_org = str(db_org[1]) + " " + str(db_org[2])
 			except IndexError:
 				db_org = line.strip().split("[", 1)
-		#			print line.strip()
-		#			print db_org[1]
 				db_org = db_org[1][:-1]
 				db_error_counter += 1
 		
 		db_org = re.sub('[^a-zA-Z0-9-_*. ]', '', db_org)
 
 		# add to dictionaries		
-#		db_func_dictionary[db_id] = db_entry
-		db_org_dictionary[db_id] = db_org
+		if "-F" in sys.argv.upper():
+			db_func_dictionary[db_id] = db_entry
+		if "-O" in sys.argv.upper():
+			db_org_dictionary[db_id] = db_org
 		
-		if db_line_counter % 1000000 == 0:									# each million
+		# line counter to show progress
+		if db_line_counter % 1000000 == 0:							# each million
 			t95 = time.clock()
 			print str(db_line_counter) + " lines processed so far in " + str(t95-t2) + " seconds."
 		
@@ -160,7 +195,10 @@ print "Number of errors: " + str(db_error_counter)
 condensed_RefSeq_hit_db = {}
 
 for entry in RefSeq_hit_count_db.keys():
-	org = db_org_dictionary[entry]
+	if "-O" in sys.argv.upper():
+		org = db_org_dictionary[entry]
+	if "-F" in sys.argv.upper():
+		org = db_func_dictionary[entry]
 	if org in condensed_RefSeq_hit_db.keys():
 		condensed_RefSeq_hit_db[org] += RefSeq_hit_count_db[entry]
 	else:
@@ -171,7 +209,10 @@ print "\nDictionary database assembled."
 print "Time elapsed: " + str(t3-t2) + " seconds."
 print "Number of errors: " + str(db_error_counter)
 
-print "\nTop ten organism matches:"
+if "-O" in sys.argv.upper():
+	print "\nTop ten organism matches:"
+if "-F" in sys.argv.upper():
+	print "\nTop ten function matches:"
 for k, v in sorted(condensed_RefSeq_hit_db.items(), key=lambda (k,v): -v)[:10]:
 	try:
 		print (str(v) + "\t" + k )
@@ -179,48 +220,27 @@ for k, v in sorted(condensed_RefSeq_hit_db.items(), key=lambda (k,v): -v)[:10]:
 		print (str(v) + "\tWARNING: Key not found for " + k)
 		continue
 
-#print "\nTop ten function matches:"
-#for k, v in sorted(RefSeq_hit_count_db.items(), key=lambda (k,v): -v)[:10]:
-#	try:
-#		print (str(v) + "\t" + db_func_dictionary[k] )
-#	except KeyError:
-#		print (str(v) + "\tWARNING: Key not found for " + k)
-#		continue
-		
 # creating the outfiles
-org_outfile_name = infile_name[:-5] + "_organism.tsv"
-#func_outfile_name = infile_name[:-4] + "_function.tsv"
+if "-O" in sys.argv.upper():
+	outfile_name = infile_name[:-5] + "_organism.tsv"
+if "-F" in sys.argv.upper():
+		outfile_name = infile_name[:-5] + "_function.tsv"
 
-org_outfile = open (org_outfile_name, "w")
-#func_outfile = open (func_outfile_name, "w")
+outfile = open (outfile_name, "w")
 
-# output for functional annotations
-#error_counter = 0
-#for k, v in sorted(RefSeq_hit_count_db.items(), key=lambda (k,v): -v):
-#	try:
-#		q = v * 100 / float(line_counter)
-#		func_outfile.write (str(q) + "\t" + str(v) + "\t" + db_func_dictionary[k] + "\n")
-#	except KeyError:
-#		func_outfile.write (str(q) + "\t" + str(v) + "\tWARNING: Key not found for " + k + "\n")
-#		error_counter += 1
-#		continue
-#
-#print "\nFunctional annotations saved to file: '" + func_outfile_name + "'."
-#print "Number of errors: " + str(error_counter)
-
-# output for organism annotations
+# writing the output
 error_counter = 0
 for k, v in sorted(condensed_RefSeq_hit_db.items(), key=lambda (k,v): -v):
 	try:
 		q = v * 100 / float(line_counter)
-		org_outfile.write (str(q) + "\t" + str(v) + "\t" + k + "\n")
+		outfile.write (str(q) + "\t" + str(v) + "\t" + k + "\n")
 	except KeyError:
-		org_outfile.write (str(q) + "\t" + str(v) + "\tWARNING: Key not found for " + k + "\n")
+		outfile.write (str(q) + "\t" + str(v) + "\tWARNING: Key not found for " + k + "\n")
 		error_counter += 1
 		continue
 
-print "\nOrganism annotations saved to file: '" + org_outfile_name + "'."
+print "\Annotations saved to file: '" + outfile_name + "'."
 print "Number of errors: " + str(error_counter)
 
 db.close()
-org_outfile.close()
+outfile.close()
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SAMSA, version 2.0
 
-Version 2 of the SAMSA pipeline - faster!  Lighter!  More options!  Less waiting!  Still in beta testing!  
+Version 2 of the SAMSA pipeline - faster!  Lighter!  More options!  Less waiting!  Still in pre-alpha testing and design!  
 
 ### New in version 2:
 * DIAMOND integration, allowing for SAMSA to be run without ever needing an MG-RAST account.
@@ -35,7 +35,34 @@ However, working with metatranscriptome data often proves challenging, given its
 **Analysis:** R scripts use DESeq to compute most significantly different features between control vs. experimental samples.  These R scripts generate a tabular output with assigned p-values and log2FoldChange scores for each feature.  These 'features' can be either organisms or specific functions.  R can also create graphs showing visual representation of the metatranscriptome(s).
 
 ### Individual programs in SAMSA v.2.0 and their functions
-(Well, there's nothing here yet.  It's still being built, but stay tuned!)
+For more information, please consult the manual, which goes into more detail on each step in the SAMSA pipeline.
+
+**Preprocessing:**
+
+* SAMSA_pre\_annotation\_pipeline.py - this program handles read trimming and merging, using FLASH (for merging mate-pairs if analyzing paired-end sequencing) and Trimmomatic for quality control.
+* SortMeRNA_example\_script.bash [shell] - example script for using SortMeRNA to remove ribosomal reads from raw data before annotation.  STILL TO COME  
+
+**Annotation:**
+
+* DIAMOND_example\_script.bash - a guide to structuring commmands for DIAMOND to perform annotations or for building dictionaries, including customizable paths to system locations and reference dictionaries.
+
+**Aggregation:**
+
+* DIAMOND_analysis\_counter.py - creates a summary file from DIAMOND results, condensing identical matches and providing counts.  Can be enabled to return either organism or functional results.  STILL TO COME
+
+**Analysis:**    
+Note: these programs are located in the "R_scripts" folder.  They all require two sets of DIAMOND summarized results from the aggregation step; an experimental set and a control set.
+
+* diversity_stats.R - this program computes Shannon and Simpson diversity scores.
+* make_DESeq\_PCA.R - this program creates a PCA plot of the supplied results (either organism or functional category).
+* make_DESeq\_heatmap.R - generates a heatmap contrasting control vs. experimental sets.
+* make_combined\_graphs.R - generates two stacked bar graphs: a relative version (y axis stretched to 100%) and an absolute version (y axis is raw counts).
+* run_DESeq\_stats.R - computes p-values and most significant differences between summarized results sets.
+
+#### Need assistance?    
+Check out the Google Group for SAMSA!  [https://groups.google.com/forum/#!forum/samsa-bioinformatics-group](https://groups.google.com/forum/#!forum/samsa-bioinformatics-group).  
+
+If you find an error with one of these programs, or simply want to ask me questions, you can contact me at [swestreich@gmail.com](mailto:swestreich@gmail.com).  
 
 #### Citations of other tools used:
 Westreich, S.T., Korf, I., Mills, D.A., Lemay, D.G.  (2016) SAMSA: A comprehensive metatranscriptome analysis pipeline.  BMC Bioinformatics.
