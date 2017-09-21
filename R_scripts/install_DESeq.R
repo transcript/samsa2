dc46d8bd47bd089357e42056d3d191f7448cb74f
mltreiber <mltreiber@ucdavis.edu>
2017-09-21 13:12:39 -0700

    Add files via upload

---
+++ R_scripts/install_DESeq.R
@@ -0,0 +1,7 @@
+# install_DESeq.R
+# Created 9/21/2017, by Michelle Treiber
+
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("DESeq2",lib=".")
+
