4d3c1fd69735291196b9cb82122226b93f03faa9
Sam Westreich <transcript@users.noreply.github.com>
2019-10-08 10:37:40 -0700

    Moving R packages to later in script

--- setup_and_test/package_installation.bash
+++ setup_and_test/package_installation.bash
@@ -31,37 +31,39 @@
 # Set pathway for SAMSA to location of samsa2 GitHub download:
 IGNORE_DEPS=1 source "${BASH_SOURCE%/*}/../bash_scripts/lib/common.sh"
 
-# 1. Download necessary R packages:
+cd $PROGRAMS
+
+# 1.  Unpack PEAR package:
+echo "Now extracting PEAR at: "; date
+tar -xvzf ${PEAR_DIR}.tar.gz
+echo "PEAR extraction finished at: "; date
+
+# 2. Unzip Trimmomatic package:
+echo "Now extracting Trimmomatic at: "; date
+unzip ${TRIMMOMATIC_DIR}.zip
+echo "Trimmomatic extraction finished at: "; date
+
+# 3. Unpack DIAMOND package
+echo "Now extracting DIAMOND at: "; date
+tar -xvzf $PROGRAMS/diamond-linux64.tar.gz
+echo "DIAMOND extraction finished at: "; date
+
+# 4. Unpack and build sortmerna package:
+echo "Now extracting SortMeRNA at: "; date
+tar -xvzf ${SORTMERNA_DIR}.tar.gz
+cd $SORTMERNA_DIR
+bash ./build.sh
+echo "SortMeRNA extraction finished at: "; date
+
+cd ../
+
+# 5. Download necessary R packages:
 echo "Now extracting R packages at: "; date
 mkdir $SAMSA/R_scripts/packages
 cd $SAMSA/R_scripts/packages
 # If using R version 3.6, change this file in the line below to install_packages_R_3.6.R .
 R --save < ../install_packages.R -v
 echo "R package extraction finished at: "; date
-
-cd $PROGRAMS
-
-# 2.  Unpack PEAR package:
-echo "Now extracting PEAR at: "; date
-tar -xvzf ${PEAR_DIR}.tar.gz
-echo "PEAR extraction finished at: "; date
-
-# 3. Unzip Trimmomatic package:
-echo "Now extracting Trimmomatic at: "; date
-unzip ${TRIMMOMATIC_DIR}.zip
-echo "Trimmomatic extraction finished at: "; date
-
-# 4. Unpack DIAMOND package
-echo "Now extracting DIAMOND at: "; date
-tar -xvzf $PROGRAMS/diamond-linux64.tar.gz
-echo "DIAMOND extraction finished at: "; date
-
-# 5. Unpack and build sortmerna package:
-echo "Now extracting SortMeRNA at: "; date
-tar -xvzf ${SORTMERNA_DIR}.tar.gz
-cd $SORTMERNA_DIR
-bash ./build.sh
-echo "SortMeRNA extraction finished at: "; date
 
 # Index silva-bac-16s-id90.fasta for sortmerna use:
 echo "Indexing the SILVA rRNA database for SortMeRNA; feel free to abort at this step if this isn't needed."
